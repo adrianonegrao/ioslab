9421ee48f5f26885cd4cda38fc9b79861b4b9b5c
Adriano Negrão <adrianonegrao@MacAdriano.caelum>
2016-07-20 21:16:19 -0300

    incluindo navegacao de tela De Lista de Contatos Para Novo Contato

--- ContatosIP67/ContatosIP67/ListaContatosViewController.m
+++ ContatosIP67/ContatosIP67/ListaContatosViewController.m
@@ -10,4 +10,25 @@
 
 @implementation ListaContatosViewController
 
+-(id)init{
+    self = [super init];
+    if(self){
+        self.navigationItem.title = @"Contatos";
+        UIBarButtonItem* botaoAdd = [[UIBarButtonItem alloc]
+                                     initWithBarButtonSystemItem: UIBarButtonSystemItemAdd
+                                     target:self
+                                     action:@selector(exibeFormulario) ];
+        
+        self.navigationItem.rightBarButtonItem = botaoAdd;
+    }
+    return self;
+}
+
+-(void)exibeFormulario{
+    UIStoryboard* board = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    FormularioContatoViewController* form = [board instantiateViewControllerWithIdentifier:@"Form-Contato"];
+    
+    [self.navigationController pushViewController:form animated:YES];
+}
+
 @end
